31773920f5d1164126c8016548c31c8c8d55376c
minami0601 <minami.love.bb@icloud.com>
2021-05-19 22:11:54 +0900

    ⚡️ 20210519

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,5 @@
 */
 
 Auth::routes();
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index'); //-- この行を編集
+Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth'); ;
